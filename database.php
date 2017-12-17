05c5c6a35573c7b3f1e666a77493efb394122ae9
Japhet Pacheco <japjaphetpacheco_14@yahoo.com>
2017-12-18 01:13:10 +0700

    connecting to database  (November 24 2017 starting of the development)

---
+++ database.php
@@ -0,0 +1,9 @@
+<?php
+    
+    $dbName = 'cinema_database' ;
+    $dbHost = 'localhost' ;
+    $dbUsername = 'root';
+    $dbUserPassword = '';
+     
+    $connection = mysqli_connect($dbHost, $dbUsername, $dbUserPassword, $dbName);
+?>
